dfe331cdef7767da4aee62b9571b99a9129b6521
Achraf Hanzaz <hanzazachraf581@gmail.com>
2025-05-05 01:12:06 +0100

    SECOND file of fixing the error

--- PetsCape/app/Http/Middleware/EnsureEmailIsVerified.php
+++ PetsCape/app/Http/Middleware/EnsureEmailIsVerified.php
@@ -4,17 +4,26 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class EnsureEmailIsVerified
 {
-    public function handle(Request $request, Closure $next)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $redirectToRoute
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next, $redirectToRoute = null)
     {
         if (! $request->user() ||
             ($request->user() instanceof MustVerifyEmail &&
-                ! $request->user()->hasVerifiedEmail())) {
-
-            return redirect()->route('verification.notice');
+            ! $request->user()->hasVerifiedEmail())) {
+            return $request->expectsJson()
+                    ? abort(403, 'Your email address is not verified.')
+                    : Redirect::route($redirectToRoute ?: 'verification.notice');
         }
 
         return $next($request);
